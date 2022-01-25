3a9a44998ed809dd3799f0af6093c9d9f2478947
Timothy Ryeyn <timothyryeyn.perez@gmail.com>
2022-01-25 13:20:42 +0800

    Created models and migrated to SQLite

--- job_portal/admin.py
+++ job_portal/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 
-from job_portal.models import Seeker
+from job_portal.models import Provider, Seeker, Job, Skill, JobSkill, SeekerSkill
 
 admin.site.register(Seeker)
+admin.site.register(Provider)
+admin.site.register(Job)
+admin.site.register(Skill)
+admin.site.register(JobSkill)
+admin.site.register(SeekerSkill)
--- job_portal/models.py
+++ job_portal/models.py
@@ -1,5 +1,50 @@
+from django.utils import timezone
 from django.db import models
-
+from django.contrib.auth.models import User
 
 class Seeker(models.Model):
-    title = models.CharField(max_length=100)
+    full_name = models.CharField(max_length=255)
+    experience = models.TextField()
+    about_me = models.TextField()
+    resume_url = models.URLField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.full_name
+
+
+class Provider(models.Model):
+    name = models.CharField(max_length=255)
+    about = models.TextField()
+    city = models.CharField(max_length=255)
+    country = models.CharField(max_length=255)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Job(models.Model):
+
+    class Type(models.IntegerChoices):
+        FULL_TIME = 1
+        PART_TIME = 2
+
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    salary = models.FloatField()
+    type = models.IntegerField(choices=Type.choices)
+    date_posted = models.DateTimeField(default=timezone.now)
+    poster = models.ForeignKey(Provider, on_delete=models.CASCADE)
+
+
+class Skill(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class SeekerSkill(models.Model):
+    seeker = models.ForeignKey(Seeker, on_delete=models.CASCADE)
+    skill = models.ForeignKey(Skill, on_delete=models.CASCADE)
+
+
+class JobSkill(models.Model):
+    job = models.ForeignKey(Job, on_delete=models.CASCADE)
+    skill = models.ForeignKey(Skill, on_delete=models.CASCADE)
+
