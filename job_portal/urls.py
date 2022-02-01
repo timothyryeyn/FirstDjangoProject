003b72562815bc185b1f5f8f82d2088fe45fa69f
Timothy Ryeyn <timothyryeyn.perez@gmail.com>
2022-02-01 10:51:27 +0800

    Done: Home Page

--- job_portal/urls.py
+++ job_portal/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('profile/jobs/create/', views.createJob, name='post_job'),
     path('profile/jobs/<int:id>/', views.showPostedJob, name='show_posted_job'),
     path('profile/jobs/<int:id>/edit', views.editJob, name='edit_job'),
+    path('profile/jobs/<int:id>/applicants/<int:application_id>', views.showPostedJobApplicant, name='show_posted_job_applicant'),
 ]
--- job_portal/views.py
+++ job_portal/views.py
@@ -6,26 +6,44 @@ from django.contrib.auth.forms import UserCreationForm
 from job_portal.models import Job
 from job_portal.forms import JobForm
 from django.http import HttpResponseNotFound
+from django.db.models import Q
 
 
 @login_required
 def home(request):
 
     jobs = Job.objects.all()
+    search = None
+    jobType = 0
 
     if (request.GET):
 
-        search = request.GET.get('search')
+        search = request.GET.get('search') if len(request.GET.get('search')) > 0 else None
+        jobType = request.GET.get('jobType')
 
-        if (search):
-            jobs = Job.objects.filter(name__contains=search).all()
+        if search:
+            jobs = Job.objects.filter(
+                Q(name__contains=search) 
+                | Q(poster__country__contains=search)
+                | Q(poster__city__contains=search)
+                | Q(jobskill__skill__name__contains=search)
+            ).all()
 
-    
+
+        if jobType:
+            jobType = int(jobType)
+            if jobType > 0:
+                jobs = jobs.filter(type=jobType).all()
+
+    pprint(jobType is '1')
+
     return render(
         request, 
         'job_portal/pages/home.html',
         {
             'jobs': jobs,
+            'search': search,
+            'jobType': jobType,
         },
     )
 
@@ -33,15 +51,17 @@ def home(request):
 @login_required
 def showJob(request, id):
     job = Job.objects.filter(id=id).first()
-
+    
     if job is None:
         return HttpResponseNotFound
 
-    seeker = request.user.seeker
-    isSeeker = seeker is not None
+    seeker = None
+    isSeeker = False
     alreadyApplied = False
 
-    if isSeeker:
+    if getattr(request.user, 'seeker', None):
+        seeker = request.user.seeker
+        isSeeker = True
         if seeker.application_set.filter(job_id=id).first():
             alreadyApplied = True
 
@@ -141,4 +161,27 @@ def showPostedJob(request, id):
         {
             'job': job,
         },
+    )
+
+
+@login_required
+def showPostedJobApplicant(request, id, application_id):
+    provider = request.user.provider
+    job = provider.job_set.filter(id=id).first()
+
+    if job is None:
+        return HttpResponseNotFound
+
+    application = job.application_set.filter(id=application_id).first()
+
+    if application is None:
+        return HttpResponseNotFound
+
+    return render(
+        request,
+        'job_portal/pages/provider/job/seeker/show.html',
+        {
+            'seeker': application.seeker,
+            'jobId': job.id
+        }
     )
